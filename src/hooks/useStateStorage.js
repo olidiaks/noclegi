584a16c8bcc33c5abb5225e7841a4da35a0b6ed4
Oliwier Stępnia <oliwierstepniak@gmail.com>
2022-08-05 12:52:07 +0200

    Fix useStateStoreage. When you want to change value to nule, webpage doesn't refres. Actualy everything working prefect.

--- src/hooks/useStateStorage.js
+++ src/hooks/useStateStorage.js
@@ -1,15 +1,17 @@
 import {useState} from "react";
 
 function useStateStorage(key, defaultValue) {
-    const [state, setState] = useState(defaultValue);
-    const storageValue = window.localStorage.getItem(key);
-    let value = storageValue ? JSON.parse(storageValue) : state;
+    const [state, setState] = useState(() => {
+        const storageValue = window.localStorage.getItem(key);
+        return storageValue ? JSON.parse(storageValue) : defaultValue;
+    });
+
     const setValue = (newValue) => {
         setState(newValue);
         window.localStorage.setItem(key, JSON.stringify(newValue));
     }
 
-    return [value, setValue];
+    return [state, setValue];
 }
 
 export default useStateStorage;
