9132a792b3f1515a5ca84864cd9b79f663f7ed1a
Oliwier Stępnia <oliwierstepniak@gmail.com>
2022-09-25 21:26:29 +0200

    Delete unnececary if statment.

--- src/components/Hotels/BestHotel/BestHotel.js
+++ src/components/Hotels/BestHotel/BestHotel.js
@@ -33,22 +33,20 @@ const BestHotel = props => {
     }, []);
 
 
-    if (!null) {
-        return (
-            <div className="card bg-success text-white">
-                <div className="card-header">Najlepsza oferta!</div>
-                <div className="card-body">
-                    <div className="d-flex justify-content-between">
-                        <h5 className="card-title no-wrap">{hotel.name}</h5>
-                        <p className="no-wrap">Ocena: {hotel.rating}</p>
-                    </div>
-                    <p>Do końca oferty pozostało: {time}</p>
-                    <Link to={`/hotele/${hotel.id}`} className="btn btn-sm btn-light">Pokaż</Link>
+    return (
+        <div className="card bg-success text-white">
+            <div className="card-header">Najlepsza oferta!</div>
+            <div className="card-body">
+                <div className="d-flex justify-content-between">
+                    <h5 className="card-title no-wrap">{hotel.name}</h5>
+                    <p className="no-wrap">Ocena: {hotel.rating}</p>
                 </div>
-
+                <p>Do końca oferty pozostało: {time}</p>
+                <Link to={`/hotele/${hotel.id}`} className="btn btn-sm btn-light">Pokaż</Link>
             </div>
-        )
-    }
+
+        </div>
+    )
 }
 
 BestHotel.propTypes = propTypes;
