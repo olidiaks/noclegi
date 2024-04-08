8affd2a56b36bb0a33110d50ef4ca628b6c739df
Oliwier Stępniak <oliwierstepniak@gmail.com>
2024-04-08 09:02:47 -0500

    Fix profiles nav bar to respect color of theme.

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import {Link, Outlet, useResolvedPath} from "react-router-dom";
+import ThemeContext from "../../context/themeContext";
 
 const Profile = () => {
     const details = useResolvedPath("szczegoly");
     const myHotels = useResolvedPath("mojeHotele");
+    const theme = useContext(ThemeContext).color;
 
 
     return (<div className="card">
@@ -13,10 +16,10 @@ const Profile = () => {
             <nav className="navbar">
                 <ul className="nav nav-tabs">
                     <li className="nav-item">
-                        <Link to={details} className="nav-link">Profil</Link>
+                        <Link to={details} className={`nav-link  text-${theme}`}>Profil</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to={myHotels} className="nav-link">Hotele</Link>
+                        <Link to={myHotels} className={`nav-link  text-${theme}`}>Hotele</Link>
                     </li>
                 </ul>
             </nav>
