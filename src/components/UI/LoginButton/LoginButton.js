19d4f1b6af539b5df36d59fb9b7d6908a95b9d96
Oliwier Stępniak <oliwierstepniak@gmail.com>
2024-04-08 08:38:53 -0500

    Add login button to follow theme of website.

--- src/components/UI/LoginButton/LoginButton.js
+++ src/components/UI/LoginButton/LoginButton.js
@@ -1,13 +1,16 @@
+import { useContext } from "react";
 import {useLocation, useNavigate} from "react-router-dom";
+import ThemeContext from "../../../context/themeContext";
 
 export function LoginButton(props) {
+    const theme = useContext(ThemeContext).color;
     const path = useLocation().pathname;
     const navigate = useNavigate();
     return (
         <button
             type="button"
             onClick={() => navigate('/zaloguj', {state: {path: path}})}
-            className="mt-2 btn btn-primary"
+            className={`mt-2 btn btn-${theme}`}
         >
             {props.children}
         </button>
